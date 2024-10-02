c4ae4f40d0cd682312c3f1bce0dce756a6cea27d
jihan-kim <jinnkenny99@gmail.com>
2024-10-02 20:51:45 +0900

    done

---
+++ jjam-v2/kubectl_conf.bash
@@ -0,0 +1,19 @@
+kubectl config use-context kind-kind
+
+#!/bin/bash
+aws eks get-token --cluster-name kind
+# Get the values from kubectl
+server=$(kubectl config view --minify --flatten --context=kind-kind -o jsonpath='{.clusters[].cluster.server}')
+certificate_authority_data=$(kubectl config view --minify --flatten --context=kind-kind -o jsonpath='{.clusters[].cluster.certificate-authority-data}')
+client_certificate_data=$(kubectl config view --minify --flatten --context=kind-kind -o jsonpath='{.users[].user.client-certificate-data}')
+client_key_data=$(kubectl config view --minify --flatten --context=kind-kind -o jsonpath='{.users[].user.client-key-data}')
+
+
+
+# Write to terraform.tfvars
+cat <<EOF > terraform.tfvars
+host                   = "${server}"
+client_certificate     = "${client_certificate_data}"
+cluster_ca_certificate = "${certificate_authority_data}"
+client_key             = "${client_key_data}"
+EOF
---
+++ jjam-v2/sched_docker/docker.sh
@@ -0,0 +1,3 @@
+docker build -t test-scheduler .
+docker tag test-scheduler jinnkenny99/test-scheduler
+docker push jinnkenny99/test-scheduler
